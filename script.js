4f924f50aec8ec616671648bdd05c495f62e068e
Apokatastasis <ramirolonatti@gmail.com>
2022-01-07 21:16:23 -0300

    Added color picker and erase and clear functions

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
-const gridContainer = document.querySelector(".grid-container");
-const eraser = document.querySelector(".eraser");
-const activeColor = "#000000";
-let clearGrid;
+let activeColor = "#000000";
+let borderColor = activeColor;
+//"gainsboro";
 
+
+const gridContainer = document.querySelector(".grid-container");
+const clearBtn = document.querySelector(".clear");
+const eraser = document.querySelector(".eraser");
+const colorPicker = document.querySelector("#color-pick");
+
+
+
+//gridContainer.addEventListener('click', paint);
+clearBtn.addEventListener('click', clear);
+colorPicker.addEventListener('change', updateColor);
+eraser.addEventListener('click', erase);
+
+function erase(){
+    activeColor = "white";
+    borderColor = "gainsboro";
+    colorPicker.value = "#FFFFFF"
+}
+
+
+
+
+
+
+function updateColor(e){
+    activeColor = e.target.value;
+    borderColor = e.target.value;
+    console.log(e.target.value);
+}
 
 
 function makeGrid(rows, columns){
@@ -10,35 +38,45 @@ function makeGrid(rows, columns){
     gridContainer.style.setProperty("--grid-columns", columns);
     for(let i=0; i< (rows*columns);i++){
     let cell = document.createElement("div");
-    gridContainer.appendChild(cell).className = "grid-cells";
+    cell.setAttribute('draggable', 'false');
+    gridContainer.appendChild(cell).className = "grid-cells gray-border";
 
     };
 }
 
-gridContainer.addEventListener('click', (e)=> {
+
+
+
+
+function paint(e){
     
-    console.log(e.target);
     e.target.style.backgroundColor = activeColor;
-    e.target.style.border = `1px solid ${activeColor}`;
+   
+    e.target.style.borderBottom = `1px solid ${borderColor}`;
+    e.target.style.borderRight = `1px solid ${borderColor}`;
     
-    console.log(e);
-})
+    
+
+}
 
 
 
-eraser.addEventListener('click', erase);
 
 
 
-function erase(){
+
+function clear(){
 
     gridItems.forEach(elem => {
     
         elem.setAttribute('style', 'background-color: white');
 
     });
+    updateColor();
     
 }
+
+
 
 
 
@@ -46,6 +84,11 @@ function erase(){
 makeGrid(20,20);
 
 const gridItems = document.querySelectorAll(".grid-cells");
+
+gridItems.forEach(elem=>{
+    elem.addEventListener('click', paint);
+})
+
 //apretar boton borrar y que borre
 //como borrar?
 //Pinto de blanco el backgroun
